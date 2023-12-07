b272450aa7c3b17107c6b2fe52bee8180d34a3b6
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-12-07 18:52:44 +0600

    eslint app config some script added

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors'
+const app: Application = express()
 
-const express = require('express')
-const app = express()
-const port = 3000
+// perser 
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req: Request, res: Response) => {
+  const a= 10;
+  res.send(a)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+export default app;
